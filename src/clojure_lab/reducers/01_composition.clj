8b981ff9d230a5bf380b31565066842e42e12596
PawelWlodarski <pwlodars@gmail.com>
2023-12-10 12:15:02 +0100

    reducers

---
+++ src/clojure_lab/reducers/01_composition.clj
@@ -0,0 +1,36 @@
+(ns clojure-lab.reducers.01-composition
+  (:require [clojure.core.reducers :as r]))
+
+;;https://clojure.org/news/2012/05/15/anatomy-of-reducer
+;;https://clojure.org/news/2012/05/08/reducers
+
+
+
+;;reducers
+(def ^:private increment-even (comp (r/filter even?) (r/map inc)))
+(comment
+  (increment-even [1 2 2 3 3 4])  ;;folder tpe
+  (doall (increment-even [1 2 2 3 3 4])) ;;coll-reduce protocol
+  (reduce + (increment-even [1 2 2 3 3 4]))
+  (into [] (increment-even [1 2 2 3 3 4])))
+
+
+;;parallel
+
+(comment 
+  (r/fold + (increment-even [1 2 2 3 3 4]))
+  (r/fold into conj (increment-even [1 1 1 2 2 3 3 4 5 5]) )
+  )
+
+;;only with standard reduce
+(comment
+  ;; error (reduce even?)
+  (let
+   [bad-reducer (partial reduce even?)
+    filtering-reducer (partial reduce (fn [r e] (if (even? e) (conj r e) r)))
+    mapping-reducer (partial reduce (fn [r e]  (conj r (inc e))))]
+    ;;(bad-reducer [1 2 2 3 4 5])
+    (filtering-reducer [] [1 2 2 3 4 5])
+    (mapping-reducer [] [1 2 2 3 4 5]))
+  )
+
